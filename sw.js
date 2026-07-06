336fa24b758ce4a94148db524c6ad5cdaef5511a
kingstatue <kingstatue@gmail.com>
2026-07-06 17:52:15 +0530

    Fix icon paths in service worker script

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const SHELL_CACHE = CACHE_VERSION + '-shell';
 
 const PRECACHE_ASSETS = [
     './manifest.json',
-    './icons/icon-192.png',
-    './icons/icon-512.png',
+    './icon-192.png',
+    './icon-512.png',
 ];
 
 function isTimetableJsonUrl(url) {
